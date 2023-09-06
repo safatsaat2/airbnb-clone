87ca45d92c9026c19d9cf966e89b2cb37d220779
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-09-07 03:38:54 +0600

    more fake data added

--- src/componants/Rooms/Rooms.jsx
+++ src/componants/Rooms/Rooms.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Card from './Card';
+import Loader from '../Shared/Loader/Loader';
 
 const Rooms = () => {
     const [rooms, setRooms] = useState([])
@@ -14,10 +15,13 @@ const Rooms = () => {
         })
         .catch(err => console.log(err))
     },[])
+    if(loading){
+        return <Loader/>
+    }
     return (
         <div className='max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4'>
             <div className='pt-12 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
-            {rooms.map(room => (<Card />))}
+            {rooms.map((room, index) => (<Card key={index} room={room}/>))}
             </div>
         </div>
     );
